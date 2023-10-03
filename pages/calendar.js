aaad839cc42ea47eeeb7c87cb5aec1340e1b5a24
eyobedk <eyobedekebede10@gmail.com>
2023-10-03 16:00:52 +0300

    full calender custom

---
+++ pages/calendar.js
@@ -0,0 +1,66 @@
+import Layout from '@/components/layout'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+import timeGridPlugin from '@fullcalendar/timegrid'
+
+export default function CalendarPage() {
+  
+const handleDateSelect = (selectInfo) => {
+  let title = prompt('Please enter a new title for your event')
+  let calendarApi = selectInfo.view.calendar
+
+  calendarApi.unselect() // clear date selection
+
+  if (title) {
+    calendarApi.addEvent({
+      id: createEventId(),
+      title,
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+      allDay: selectInfo.allDay
+    })
+  }
+}
+
+  return (
+    <Layout>
+      <div className='calendar-container'>
+        <FullCalendar
+          plugins={[
+            resourceTimelinePlugin,
+            dayGridPlugin,
+            interactionPlugin,
+            timeGridPlugin
+          ]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+          initialView='dayGridMonth'
+          nowIndicator={true}
+          editable={true}
+          selectable={true}
+          select={handleDateSelect}
+          selectMirror={true}
+          resources={[
+            { id: 'a', title: 'Auditorium A' },
+            { id: 'b', title: 'Auditorium B', eventColor: 'green' },
+            { id: 'c', title: 'Auditorium C', eventColor: 'orange' },
+          ]}
+          initialEvents={[
+            { title: 'nice event', start: new Date(), resourceId: 'a' }
+          ]}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+let eventGuid = 0
+
+export function createEventId() {
+  return String(eventGuid++)
+}
